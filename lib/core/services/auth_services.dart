83e1ded95c1fdcd5992c42366efb1129e3ce5bf6
aghni <aghniihsn.gmail.com>
2026-02-10 21:29:17 +0700

    url

--- lib/core/services/auth_services.dart
+++ lib/core/services/auth_services.dart
@@ -9,11 +9,13 @@ class AuthService {
 
   Future<Map<String, dynamic>?> login(String email, String password) async {
     try {
-      final response = await http.post(
-        Uri.parse('$baseUrl/login'),
-        headers: {'Content-Type': 'application/json'},
-        body: jsonEncode({'email': email, 'password': password}),
-      );
+      final response = await http
+          .post(
+            Uri.parse('$baseUrl/login'),
+            headers: {'Content-Type': 'application/json'},
+            body: jsonEncode({'email': email, 'password': password}),
+          )
+          .timeout(const Duration(seconds: 10));
 
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
